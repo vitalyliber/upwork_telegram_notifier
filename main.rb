a61d68c1a8ae2b80dcc8d47155fea9c57880156b
Vitaly Liber <zenamax@gmail.com>
2019-07-03 16:10:34 +0300

    return the previous state

--- main.rb
+++ main.rb
@@ -16,7 +16,7 @@ channels = [
     {
         url: 'https://www.upwork.com/ab/feed/jobs/rss?contractor_tier=2%2C3&proposals=0-4&q=ruby+on+rails&sort=recency&job_type=hourly&pa',
         channel: 'upwork_ror_notifier',
-        cold_start: false,
+        cold_start: true,
     },
     {
         url: 'https://www.upwork.com/ab/feed/jobs/rss?proposals=0-4&q=react+native&sort=recency&job_type=hourly&paging=0%3B10&api_params=',
