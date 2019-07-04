dee0f6023dd431396817eaeeaa27681b2d0eec8e
Vitaly Liber <zenamax@gmail.com>
2019-07-04 14:33:14 +0300

    reconfigured =)

--- README.md
+++ README.md
@@ -11,7 +11,9 @@ bundle install
 Set environment variables in the .env file or globally
 
 ```
-INSTAGRAM_API_TOKEN=xxx:xxx
+TELEGRAM_API_TOKEN=xxx:xxx
+RSS_LINKS="https://www.upwork.com/ab/feed/jobs/rss?contractor_tier=2%2C3&proposals=0-4&q=ruby+on+rails&sort=recency&job_type=hourly&pa https://www.upwork.com/ab/feed/jobs/rss?proposals=0-4&q=react+native&sort=recency&job_type=hourly&paging=0%3B10&api_params="
+TELEGRAM_CHANNELS="upwork_ror_notifier upwork_rn_notifier"
 ```
 
 Run the UpWork notifier
@@ -19,3 +21,10 @@ Run the UpWork notifier
 ```
 ruby main.rb
 ```
+
+## How to configure telegram channel
+
+- Create a Telegram public channel
+- Create a Telegram BOT via BotFather
+- Set the bot as administrator in your channel
+- Set the bot api token to environment variable `TELEGRAM_API_TOKEN`
--- main.rb
+++ main.rb
@@ -6,21 +6,18 @@ require 'nokogiri'
 p "Program started"
 
 time_interval = 15
-@instagram_api_token = ENV.fetch("INSTAGRAM_API_TOKEN")
+@telegram_api_token = ENV.fetch("TELEGRAM_API_TOKEN")
 
 @checked = {}
-channels = [
-    {
-        url: 'https://www.upwork.com/ab/feed/jobs/rss?contractor_tier=2%2C3&proposals=0-4&q=ruby+on+rails&sort=recency&job_type=hourly&pa',
-        channel: 'upwork_ror_notifier',
-        cold_start: true,
-    },
-    {
-        url: 'https://www.upwork.com/ab/feed/jobs/rss?proposals=0-4&q=react+native&sort=recency&job_type=hourly&paging=0%3B10&api_params=',
-        channel: 'upwork_rn_notifier',
-        cold_start: true
-    }
-]
+rss_links = ENV.fetch("RSS_LINKS") {''}.split(' ')
+telegram_channels = ENV.fetch("TELEGRAM_CHANNELS") {''}.split(' ')
+channels = rss_links.each_with_index.map do |channel, index |
+  {
+      channel: telegram_channels[index],
+      url: channel,
+      cold_start: true
+  }
+end
 
 def check_rss(channel)
   rss = open(channel[:url])
@@ -35,7 +32,7 @@ def check_rss(channel)
     end
     plain_text = Nokogiri::HTML(item.description).text
     plain_text = "#{item.link}\n\n#{plain_text}"
-    instagram_channel_url = "https://api.telegram.org/bot#{@instagram_api_token}/sendMessage?chat_id=@#{channel[:channel]}&text=#{plain_text}"
+    instagram_channel_url = "https://api.telegram.org/bot#{@telegram_api_token}/sendMessage?chat_id=@#{channel[:channel]}&text=#{plain_text}"
     instagram_channel_url = URI.parse(URI.escape(instagram_channel_url))
     open(instagram_channel_url)
   end
